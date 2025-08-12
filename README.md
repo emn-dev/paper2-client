25fa76ccec4145244bc0d91e3d0c3b0465729b6b
Rick B. <richardb.barton@eastman.com>
2025-08-12 08:48:54 -0500

    init

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # paper2-client
-Testing client for paper2
+
+Testing client for [paper2](https://github.com/emn-dev/paper2). Easy place to import and show parity or differences between paper2 vs [paperjs](https://github.com/paperjs/paper.js). Currently only concerned with nodejs functionality as you can view browser examples in the [docs](https://emn-dev.github.io/paper2/).
+
+## Getting Started
+
+- Require nodejs version >= `v22` ([Node.js Releases](https://nodejs.org/en/about/previous-releases))
+- NOTE: Using `type=module` in package.json [module-vs-commonjs](https://nodejs.org/api/packages.html#type)
+- Run `npm install`
+
+## Testing
+
+- Testing paper2 = `npm start`
+- Testing paperjs (original) = `npm run start-paperjs`
+
+## jsdom
+
+- jsdom in this project is stuck at `v20.0.3` because of a bug in [paperjs - 2058](https://github.com/paperjs/paper.js/pull/2058)
+- You can bump jsdom to latest and paper2 will work correctly, but paperjs will throw error
---
+++ index-paperjs.js
@@ -0,0 +1,43 @@
+import paper from "paper/dist/paper-core.js";
+
+const mySvg = `
+<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" width="10cm" height="30.48cm" viewBox="0 0 10 30.48">
+   <g id="export">
+      <g id="foo">
+         <g id="bar">
+            <path id="blah" d="M1.13313,25.99574v0h0.38994v0v0.71488v0h-0.38994v0z" />
+         </g>
+      </g>     
+   </g>
+</svg>
+`;
+
+const proj = new paper.Project(new paper.Size(1, 1));
+
+console.log("----- CODE_BLOCK_1 -----");
+
+try {
+  const layer = proj.importSVG(mySvg); // Cannot do without JSDOM
+  const items = layer.children["export"].getItems({ recursive: true });
+  items.forEach((item) => {
+    console.log(item.className, item.name);
+  });
+} catch {
+  console.log("CANNOT run this code without JSDOM");
+}
+
+console.log("----- CODE_BLOCK_2 -----");
+
+// This works with or without JSDOM
+const path = new paper.Path([100, 100], [100, 200]);
+path.name = "foo2";
+const path2 = new paper.Path([50, 150], [150, 150]);
+path2.name = "bar2";
+const layer2 = new paper.Layer({
+  children: [path, path2],
+  strokeColor: "black",
+  position: paper.view.center,
+});
+layer2.children.forEach((item) => {
+  console.log(item.className, item.name);
+});
